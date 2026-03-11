08ee9dcd320f700f217efcc951f71dc3e7d3d93d
Qwazilio <fghjrsdvh@gmail.com>
2026-03-11 17:33:56 +0300

    fix connection with DB

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,24 @@
-
-import { Injectable } from '@nestjs/common';
-import {PrismaClient} from "@prisma/client";
+import {Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import {PrismaPg} from "@prisma/adapter-pg";
 
-
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     constructor() {
-        const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
-        super({ adapter });
+        super({
+            adapter: new PrismaPg({
+                connectionString: process.env.DATABASE_URL
+            }),
+            errorFormat: 'pretty',
+            log: ['error', 'warn']
+        });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
     }
 }
