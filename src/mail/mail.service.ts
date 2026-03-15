81ba40c7a3d727b9b908bf8ec99d7b0e82130782
Qwazilio <51093969+Qwazilio@users.noreply.github.com>
2026-03-15 12:51:58 +0300

    smtp mail

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import { Transporter } from 'nodemailer';
+import * as process from 'node:process';
 
 @Injectable()
 export class MailService {
-  async sendMail(to: string, subject: string, body: string) {
+  private transporter: Transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST || 'smtp.gmail.com',
+    port: Number(process.env.SMTP_PORT) || 587,
+    secure: process.env.SMTP_SECURE === 'true',
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
 
+  async sendMail(to: string, subject: string, body: string) {
+    //ТУТ можно в боди какую нибудь доп инфу.
+    await this.transporter.sendMail({
+      from: `Simple service`,
+      to,
+      subject,
+      html: body,
+    });
   }
 }
