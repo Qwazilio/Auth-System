ff4425a661dfcbd4d0c15d48f60020b3f53742e5
Qwazilio <51093969+Qwazilio@users.noreply.github.com>
2026-03-16 07:39:57 +0300

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,13 @@
-import {Controller, Get, Param, ParseIntPipe, Post} from '@nestjs/common';
-import {UserService} from "./user.service";
-import {Public} from "../common/decorators/public.decorator";
-import {User} from "@prisma/client";
+import { Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-    @Get(':id')
-    async getUser(@Param('id', ParseIntPipe) id: number): Promise<Partial<User>> {
-        return this.userService.getUser(id);
-    }
+  // @Get(':id')
+  // async getUser(@Param('id', ParseIntPipe) id: number): Promise<Partial<User>> {
+  //   return this.userService.getUser(id);
+  // }
 }
